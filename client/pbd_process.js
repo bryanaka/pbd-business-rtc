ac90e582baf67f826c70184f077b4e3f4a31f80e
Bryan M. Robles <bryanaka0@gmail.com>
2013-10-11 15:57:06 -0700

    added a ton of stuff, including adding a new request and deleting a request

--- client/pbd_process.js
+++ client/pbd_process.js
@@ -1,17 +1,22 @@
-Template.login.events({
+Template.sessions_ui.events({
   'click #loginPersona': function () {
     Meteor.loginWithPersona({}, function() {
-      console.log('logged in!');
+      console.log('logged in with Persona!');
     });
-  }
-});
-
-Template.logout.events({
+  },
   'click #logOut': function () {
     Meteor.logout();
   }
 });
 
 Template.travel.travel_requests = function() {
-   return TravelRequests.find();
+  return TravelRequests.find();
 };
+
+var TravelRequestsCollection = function() {
+  return TravelRequests;
+};
+
+Template.travel.TravelRequestsCollection = TravelRequestsCollection;
+Template.new_travel.TravelRequestsCollection = TravelRequestsCollection;
+Template.edit_travel.TravelRequestsCollection = TravelRequestsCollection;
--- client/router.js
+++ client/router.js
@@ -13,12 +13,38 @@ Router.configure({
 
 
 Router.map(function() {
+
   this.route('home', {
-	path: '/',
-	data: TravelRequests.find(),
+  path: '/',
     template: 'travel',
     renderTemplates: {
-      'topNav': {to: 'topNav'}
+      'travel_nav': {to: 'topNav'}
     }
   });
+
+  this.route('travel', {
+    path: '/travel',
+    template: 'travel',
+    renderTemplates: {
+      'travel_nav': {to: 'topNav'}
+    }
+  });
+
+  this.route('new_travel', {
+    path: '/travel/new',
+    template: 'new_travel',
+    renderTemplates: {
+      'travel_nav': {to: 'topNav'}
+    }
+  });
+
+  this.route('show_travel', {
+    path: '/travel/:_id',
+    data: function() { return TravelRequests.findOne(this.params._id); },
+    template: 'show_travel',
+    renderTemplates: {
+      'travel_nav': {to: 'topNav'}
+    }
+  });
+
 });
